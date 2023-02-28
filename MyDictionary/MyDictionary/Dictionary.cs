8e4fb38eaf469c5b8dff0631fc08c40142ba2de0
mustafauzun <mustafauzun_must@hotmail.com>
2023-02-28 21:06:57 +0300

    MyDictionary
    Dictionary örnek

---
+++ MyDictionary/MyDictionary/Dictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Dictionary<TKey,TValue>
+    {
+        TKey[] Id;
+        TValue[] Name;
+        TKey[] tempId;
+        TValue[] tempName;
+
+        public Dictionary()
+        {
+            Id = new TKey[0];
+            Name = new TValue[0];
+        }
+
+        public void Add(TKey id, TValue name)
+        {
+            tempId = Id;
+            tempName = Name;
+
+            Id = new TKey[Id.Length + 1];
+            Name = new TValue[Name.Length + 1];
+
+            for (int i = 0; i < tempId.Length; i++)
+            {
+                Id[i] = tempId[i];
+
+                Name[i] = tempName[i];
+
+            }
+
+            Id[Id.Length - 1] = id;
+            Name[Id.Length - 1] = name;
+
+
+        }
+
+
+
+
+
+
+        public TKey[] CountId
+        {
+            get { return Id; }
+        }
+
+        public TValue[] CountName
+        {
+            get { return Name; }
+        }
+
+        public int NameLenght
+        {
+            get { return Name.Length; }
+        }
+    }
+
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<int,string> dictionary = new Dictionary<int,string>();
+
+            dictionary.Add(1,"Mustafa");
+            dictionary.Add(2,"Engin");
+            dictionary.Add(3,"Kemal");
+
+            for (int i = 0; i < dictionary.NameLenght; i++)
+            {
+                Console.WriteLine("Sıra No :" + dictionary.CountId[i]+" "+
+                                   "Adi : " + dictionary.CountName[i]);
+            }
+            Console.WriteLine("Mevcut liste sayısı : " + dictionary.NameLenght);
+        }
+    }
+}
